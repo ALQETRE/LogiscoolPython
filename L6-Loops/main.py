fad4066a95c088a44d0d3b4f7e4305c4280cba30
ALQETRE <alqetrea@gmail.com>
2026-03-26 18:16:14 +0100

    L6

---
+++ L6-Loops/main.py
@@ -0,0 +1,88 @@
+# # int x
+# # float x
+# # bool x
+
+# # string *
+# # tuple **
+# # list *
+
+# # dict x *
+
+
+# people = {
+#     "alice": 10,
+#     "bob": 13
+# }
+
+
+# for name, age in people.items():
+#     print(name)
+#     print(age)
+
+# print("end")
+
+# # print() -> funkce
+# # text = "Hi" -> Variable
+# # for -> keyword
+
+
+# import random
+
+
+# weather = "sunny"
+
+# while weather == "sunny":
+#     print("sunny")
+
+#     if random.randint(0, 1) == 1:
+#         weather = "rainy"
+
+
+
+
+
+
+
+
+# def gameEnd():
+#     user_input = input("End? ")
+#     if user_input == "yes":
+#         return True
+#     else:
+#         return False
+
+# while True:
+#     print("hi")
+
+#     if gameEnd():
+#         break
+
+# print("End")
+
+
+
+
+for i in range(10):
+    print(i)
+
+
+print()
+
+i = 0
+while i < 10:
+    print(i)
+
+    i+=1
+
+
+print()
+
+
+i = 0
+while True:
+    print(i)
+
+    i+=1
+
+    if i > 9:
+        break
