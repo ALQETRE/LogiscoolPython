8a62531bffbf1d80df6077904d7b9ae966636345
ALQETRE <alqetrea@gmail.com>
2026-03-26 18:16:10 +0100

    L5-Ex

---
+++ L5-DataTypes(Ex)/main.py
@@ -0,0 +1,21 @@
+# int - 123
+# float - 3.14
+# str - "Hello"
+# bool - True
+# list - ["Ahoj", "jak"]
+# tuple - (1, 2, 3, 4)
+# dict - {"name": "hi", "age": 10}
+
+
+text = "123"
+
+print( int(text) + 2)
+
+print(str(123)*3)
+
+list((1, 2, "Ahoj", True))
+
+print(bool(""))
+
+
+dict()
